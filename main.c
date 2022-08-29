55d81e57671fcc685436c59b9188576e8027b26f
fan <fanrc96@outlook.com>
2022-08-29 15:10:05 +0900

    try using uefi_call_wrapper

--- main.c
+++ main.c
@@ -5,6 +5,9 @@ EFI_STATUS
 EFIAPI
 efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
   InitializeLib(ImageHandle, SystemTable);
-  Print(L"Hello, world!\n");
+  uefi_call_wrapper(ST->ConOut->OutputString, 2, ST->ConOut,
+                    L"Hello, world!\n");
+  Print(L"%s : %d\n", SystemTable->FirmwareVendor,
+        SystemTable->FirmwareRevision);
   return EFI_SUCCESS;
 }
